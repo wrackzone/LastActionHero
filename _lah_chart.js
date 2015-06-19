d40262a189999b172d592d1564e780aec9ee6916
Barry Mullan <mullan@gmail.com>
2015-06-19 10:40:34 -0700

    fixed cache issue

---
+++ _lah_chart.js
@@ -0,0 +1,44 @@
+Ext.define('Rally.technicalservices.lahChart',{
+    extend: 'Rally.ui.chart.Chart',
+    alias: 'widget.progresschart',
+
+    itemId: 'rally-chart',
+    chartData: {},
+    loadMask: false,
+    chartColors : [],
+    chartConfig: {
+        colors : ["#E0E0E0","#00a9e0","#fad200","#8dc63f"],
+        chart: {
+            type: 'bubble'
+        },
+        title: {
+            text: 'Progress by Project'
+        },
+        xAxis: {
+                title: {
+                    text: 'Median Feature TIP'
+                }
+        },
+        yAxis: [
+            {
+                title: {
+                    text: '# Portfolio Items'
+                }
+            }
+        ],
+        plotOptions: {
+            bubble: {
+                tooltip: {
+                    headerFormat: '<b>{series.name}</b><br>',
+                    pointFormat: 'TIP:{point.x}, Portfolio Items:{point.y}, Users:{point.z}'
+                }
+            }
+        }
+    },
+    constructor: function (config) {
+        this.callParent(arguments);
+        if (config.title){
+            this.chartConfig.title = config.title;
+        }
+    }
+});
