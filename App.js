f0f1e18261d839eda7f5f82ec6a554f27c1c049c
Barry Mullan <mullan@gmail.com>
2015-06-19 10:13:40 -0700

    chart

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
+
     launch: function() {
 
     	var me = this;
@@ -10,9 +11,22 @@ Ext.define('CustomApp', {
                 console.log("results",results);
                 var workspaces = _.first(results);
                 var permissions = results[1];
-                me.setUserCounts(workspaces,permissions);
-                // me.setProjectCounts(workspaces);
-                me.setFeatureCounts(workspaces);
+
+                Deft.Promise.all([
+
+                	me.setUserCounts(workspaces,permissions),
+                	me.setFeatureCounts(workspaces),
+                	me.setFeatureTIP(workspaces)
+
+                ],me).then({
+
+                	success : function(results) {
+						console.log("workspaces",_.map(workspaces,function(w){return w.get("Name")+"-"+w.get("UserWorkspaceCount")+":"+w.get("ProjectsCount")}));
+						me.createChart(me.prepareChartData(workspaces));
+                	}
+
+                });
+
             }
         });
     },
@@ -38,43 +52,72 @@ Ext.define('CustomApp', {
 
     prepareChartData : function(workspaces) {
 
-    	var seriesData = _.map(workspaces,function(ws){
-    		return {
-    			name : ws.get("Name"),
-    			y : ws.get("WorkspaceFeatureCount"), x : 100, z : ws.get("UserWorkspaceCount")
-    		}
-    	});
+		var seriesData = _.map(workspaces,function(ws){
+			return {
+				name : ws.get("Name"),
+				data : [{ x : ws.get("FeatureTIPValue"), 
+						  y : ws.get("WorkspaceFeatureCount"), 
+						  z : ws.get("UserWorkspaceCount")}]
+			}
+		});    	
 
-    	var series = [{
-    		data : seriesData
-    	} ]
-
-    	console.log("seriesData",series);
-    	return series;
+    	console.log("seriesData",seriesData);
+    	return seriesData;
 
     },
 
     setFeatureCounts : function(workspaces) {
+    	console.log("Getting Features");
     	var me = this;
+    	var deferred = Ext.create('Deft.Deferred');
     	me.readPortfolioItems(workspaces).then({
 			success : function(values) {
 				console.log("values",values);
 				_.each(workspaces,function(workspace,i){
 					workspace.set("WorkspaceFeatureCount",values[i].length);
 				})
-				console.log("workspaces",_.map(workspaces,function(w){return w.get("Name")+"-"+w.get("UserWorkspaceCount")+":"+w.get("ProjectsCount")}));
-				me.createChart(me.prepareChartData(workspaces));
+				deferred.resolve([]);
 			},
 			failure : function(error) { 
 				console.log("Error:",error);
 			}
 		});
+		return deferred.promise;
+    },
+
+    setFeatureTIP : function(workspaces) {
+    	console.log("Getting Feature TIP");
+
+    	var me = this;
+    	var deferred = Ext.create('Deft.Deferred');
+
+    	var getLastTIPFromValue = function(value) {
+    		var scope = _.first(value.scopes);
+    		if (scope.dataPoints.length===0) // no metric
+    			return 0;
+    		var datapoint = _.last(scope.dataPoints);
+    		var metricValueKey = _.first(_.keys(datapoint.data));
+    		var metricValue = datapoint.data[metricValueKey].value;
+    		return metricValue;
+    	};
+
+    	me.readFeatureMetrics(workspaces).then({
+    		success : function(values) {
+    			console.log("Workspace Insights API:",values);
+    			_.each(workspaces,function(workspace,i){
+    				workspace.set("FeatureTIPValue", getLastTIPFromValue(values[i]));
+    			})
+    			deferred.resolve([]);
+    		}
+    	});
+    	return deferred.promise;
+
     },
 
     setProjectCounts : function(workspaces) {
 
     	var me = this;
-
+    	var deferred = Ext.create('Deft.Deferred');
     	me.readProjects(workspaces).then({
     		success : function(values) {
     			console.log("project values",values);
@@ -82,19 +125,25 @@ Ext.define('CustomApp', {
     				workspace.set("WorkspaceProjects",values[i]);
     				workspace.set("ProjectsCount",values[i].length);
     			})
+    			deferred.resolve([]);
     		}
-    	})
+    	});
+    	return deferred.promise;
 
     },
 
     setUserCounts : function(workspaces,permissions) {
+    	console.log("Getting Users");
+    	var deferred = Ext.create('Deft.Deferred');
         _.each(workspaces,function(ws,i){
 	    	var perms = _.filter(permissions,function(perm){
-	    		return (perm.get("Workspace")._ref === ws.get("_ref")) &&
-	    			(perm.get("Role") === "User");
+	    		return (perm.Workspace._ref === ws.get("_ref")) &&
+	    			(perm.Role === "User");
 	    	});
 	    	ws.set("UserWorkspaceCount",perms.length);
 	    });
+	    deferred.resolve([]);
+	    return deferred.promise;
     },
 
     readProjects : function(workspaces) {
@@ -162,20 +211,99 @@ Ext.define('CustomApp', {
         return Deft.Promise.all(promises);
 	},
 
+	readFeatureMetrics : function(workspaces) {
+
+		var insightsAPI = function(workspace) {
+			var deferred = Ext.create('Deft.Deferred');
+			var metrics = [
+            	"TimeInProcessFeatureStart0P50"
+            ];
+			var workspace = workspace.get("ObjectID");
+			var lastMonth = moment(); // moment().subtract(1,'months');
+			var firstMonth = moment().subtract(6,'months');
+
+			var baseUrl = "https://rally1.rallydev.com/insight/data";
+			// var baseUrl = "/insight/data";
+			var granularity = "month";
+
+			var url = baseUrl + "?" + "granularity=" + granularity + "" + "&metrics=" + metrics.join() + 
+						// "&projects=" + project + 
+						"&workspaces=" + workspace + 
+						"&start-date=" + firstMonth.format("YYYY-MM") + 
+						"&end-date=" + lastMonth.format("YYYY-MM");
+
+			Ext.Ajax.request({
+		   		url: url,
+		   		success: function(response, opts) {
+			      var obj = Ext.decode(response.responseText);
+			      deferred.resolve(obj);
+			   	},
+		   		failure: function(response, opts) {
+		      		console.log('server-side failure with status code ' + response.status);
+		   		}
+			});
+			return deferred.promise;
+		};
+
+		var me = this;
+        var promises = _.map(workspaces,function(workspace) {
+        	return insightsAPI(workspace);
+        });
+        return Deft.Promise.all(promises);
+	},
 
 	readWorkspacePermissions : function() {
+
+		var me = this;
+        
         var deferred = Ext.create('Deft.Deferred');
-        this._loadAStoreWithAPromise(
-            'WorkspacePermission', 
-            ["Workspace","User","Name","Role"]
-            ).then({
-            scope: this,
-            success: function(permissions) {
-            	console.log("permissions",permissions);
-                deferred.resolve(permissions);
-            }
+
+        Ext.define('lah-cache', {
+		    fields: ['date', 'value'],
+		    extend: 'Ext.data.Model',
+		    proxy: {
+		        type: 'localstorage',
+		        id  : 'lah-workspace-permissions'
+		    }
+		});
+
+		var store = Ext.create('Ext.data.Store', {
+			autoLoad : false,
+    		model: "lah-cache"
+		});
+
+		store.load(function(records, operation, success){
+
+			console.log("Success:",success,"Records:",records.length,records);
+			var now = moment();
+			if (records.length>0) {
+				var rec = _.first(records);
+				if (moment.duration(now.diff( moment(rec.get("date")))).asHours() > 24) {
+					store.remove(records);
+					store.sync();
+				} else {
+					console.log("Resolving from cache");
+					deferred.resolve(_.first(records).get("value"));
+				}
+			} else {
+		        me._loadAStoreWithAPromise(
+	            'WorkspacePermission', 
+	            ["Workspace","User","Name","Role"]
+	            ).then({
+		            scope: this,
+		            success: function(permissions) {
+		            	console.log("Permissions:",permissions.length);
+		            	var data = _.map(permissions,function(p){return p.data;});
+		            	store.add({date: new Date(),value: data});
+		            	store.sync();
+		                deferred.resolve(permissions);
+		            }
+	            }) 
+	        } 
         });
+
         return deferred.promise;
+    
     },
 
     readWorkspaces : function() {
@@ -211,106 +339,6 @@ Ext.define('CustomApp', {
         });
     	return deferred.promise;
     },
-
-    readWorkspaceFeatures : function(workspaces) {
-
-        var createFeatureFilter = function() {
-            var filter = Ext.create('Rally.data.wsapi.Filter', {
-                property: 'CreationDate',
-                operator: '>',
-                value: "2015-01-01T00:00:00.000Z"
-            });
-        	return filter;
-        };
-
-    	var me = this;
-        var promises = _.map(workspaces,function(workspace) {
-            var deferred = Ext.create('Deft.Deferred');
-	         me._loadAStoreWithAPromise(
-                'PortfolioItem', 
-                ["ObjectID"], 
-                [createFeatureFilter()],
-                // [ { property:"Ordinal", operator:"=", value:0} ],
-                {
-                	project : null,
-                	workspace : workspace.get("_ref")
-                }
-                ).then({
-                scope: me,
-                success: function(pis) {
-                    deferred.resolve(pis);
-                }
-            });
-            return deferred.promise;
-        });
-        return Deft.Promise.all(promises);
-    },
-
-    readFeatures : function(workspace) {
-
-        var me = this;
-
-        var createFeatureFilter = function() {
-            var filter = Ext.create('Rally.data.wsapi.Filter', {
-                property: 'CreationDate',
-                operator: '>',
-                value: "2015-01-01T00:00:00.000Z"
-            });
-        	return filter;
-        };
-
-        var readFeatureType = function() {
-            var deferred = Ext.create('Deft.Deferred');
-            me._loadAStoreWithAPromise(
-                'TypeDefinition', 
-                ["TypePath"], 
-                [ { property:"Ordinal", operator:"=", value:0} ],
-                {
-                	project : null,
-                	workspace : workspace.get("_ref")
-                }
-                ).then({
-                scope: me,
-                success: function(types) {
-                    deferred.resolve(_.first(types).get("TypePath"));
-                }
-            });
-            return deferred.promise;
-        };
-
-        var readFeatures = function(type) {
-        	console.log("Reading:",type);
-
-        	var deferred = Ext.create('Deft.Deferred');
-            me._loadAStoreWithAPromise(
-                type, 
-                ["FormattedID","Name","ObjectID","CreationDate"], 
-                [createFeatureFilter()],
-                {   
-                	project: null,
-                    workspace : workspace.get("_ref")
-                }
-            ).then({
-            	scope: me,
-                success: function(features) {
-                	console.log("Features:",features.length)
-                    deferred.resolve(features);
-                }
-        	});
-            return deferred.promise;
-        };
-
-        var deferred = Ext.create('Deft.Deferred');
-        Deft.Chain.pipeline([readFeatureType,readFeatures],self).then({
-            success: function(results) {
-                deferred.resolve(results);
-            },
-            failure: function(error) {
-            	console.log("error:",error);
-            }
-        });
-        return deferred.promise;
-	},
 
     _loadAStoreWithAPromise: function(model_name, model_fields, filters,ctx,order) {
         var deferred = Ext.create('Deft.Deferred');
